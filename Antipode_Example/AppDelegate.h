5a6f584c131061beee048d0fede53e3612606a76
Vicente de Miguel <spidvmp@gmail.com>
2015-03-25 19:57:58 +0100

    Initial Commit

---
+++ Antipode_Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Antipode_Example
+//
+//  Created by Vicente de Miguel on 25/3/15.
+//  Copyright (c) 2015 Nicatec Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
